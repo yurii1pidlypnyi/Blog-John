8e4b3958fecee2e60d19073b4b8315344714a1bb
Yurii <pidlypniyuyra@gmai.com>
2021-11-09 11:06:18 +0200

    Адаптивна верстка шапки

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Адаптив шапки
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+let body = document.body;
+
+burger.addEventListener('click',()=>{
+  burger.classList.toggle('active')
+  menu.classList.toggle('active')
+  body.classList.toggle('lock')
+})
+//----------------------------------
